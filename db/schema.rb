6caa3245f9ee651cf6e5054c15b89cc193ddb4e0
tkudlek <tmk54@txstate.edu>
2017-11-03 11:16:10 -0500

    Database Switch
    Database type is now mySQL, so 'gem mysql2' was added, and the
    database.yml has been edited to use the mysql adapter.

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 20171015143610) do
 
-  create_table "reviews", force: :cascade do |t|
+  create_table "reviews", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
